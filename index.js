1cab491f33b27bc7414c6a80cf3922375b0925cf
nico8a54 <nico8a54@hotmail.com>
2021-09-14 21:11:17 -0500

    v3
    Add js to caroussel

---
+++ index.js
@@ -0,0 +1,18 @@
+const card1 = document.getElementById("cardProduct1");
+// const card2 = document.querySelector('.cardProduct2');
+// const card3 = document.querySelector('.cardProduct3');
+// const card4 = document.querySelector('.cardProduct4');
+// const card5 = document.querySelector('.cardProduct5');
+const slider = document.querySelector('.slider');
+
+
+function slide () {
+   slider.style.transform = "translateX(-75vw)";
+}
+card1.addEventListener("touchmove", slide);
+
+// card1.addEventListener ("click",slide)
+// card2.addEventListener ("click",slide)
+// card3.addEventListener ("click",slide)
+// card4.addEventListener ("click",slide)
+// card5.addEventListener ("click",slide)
