fdab8810ecf2259b665a2557b97d2f6c8e505897
nico8a54 <nico8a54@hotmail.com>
2021-09-21 17:15:21 -0500

    v10
    Se separaron las paginas para mejor navegabilidad

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
-const card1 = document.getElementById("cardProduct1");
-// const card2 = document.querySelector('.cardProduct2');
-// const card3 = document.querySelector('.cardProduct3');
-// const card4 = document.querySelector('.cardProduct4');
-// const card5 = document.querySelector('.cardProduct5');
-const slider = document.querySelector('.slider');
+const footer = document.getElementById("footer"); 
+const footerH4 = document.createElement("h4");
+const footerLogoContainer = document.createElement ("div");
+const footerLogoYoutube = document.createElement ("a");
+const footerLogoFacebook = document.createElement ("a");
+const footerLogoInstagram = document.createElement ("a");
+const footerLogoPinterest = document.createElement ("a");
+const spanYoutube = document.createElement ("span");
+const spanFacebook = document.createElement ("span");
+const spanInstagram = document.createElement ("span");
+const spanPinterest = document.createElement ("span");
+const footerH3 = document.createElement ("h3");
+const footerOtrosSitios = document.createElement ("div");
+const coronaCo = document.createElement ("a");
+const centroCorona = document.createElement("a");
+const vajillasCorona = document.createElement("a");
+const inspirame = document.createElement("a");
+
+footerH4.innerHTML = "construimos sueños a través de los espacios";
+
+footerLogoYoutube.appendChild(spanYoutube);
+spanYoutube.classList.add("icon"); 
+spanYoutube.classList.add("youtube"); 
+footerLogoYoutube.href = "https://youtube.com";
+
+footerLogoFacebook.appendChild(spanFacebook);
+spanFacebook.classList.add("icon");
+spanFacebook.classList.add("facebook");
+footerLogoFacebook.href = "https://facebook.com"
+
+footerLogoInstagram.appendChild(spanInstagram);
+spanInstagram.classList.add("icon");
+spanInstagram.classList.add("instagram");
+footerLogoInstagram.href = "https://instagram.com"
+
+footerLogoPinterest.appendChild(spanPinterest);
+spanPinterest.classList.add("icon");
+spanPinterest.classList.add("pinterest");
+footerLogoPinterest.href = "https://pinterst.com";
+
+footerLogoContainer.append(footerLogoYoutube, footerLogoFacebook, footerLogoInstagram, footerLogoPinterest);
+
+footerH3.innerHTML = "Visita nuestros otros sitios";
+coronaCo.href = "https://corona.co";
+coronaCo.innerText = "Corona.co";
+centroCorona.href ="";
+centroCorona.innerText = "Centros Corona";
+vajillasCorona.href = "";
+vajillasCorona.innerText = "Vajillas Corona";
+inspirame.href = "";
+inspirame.innerText = "Inspirame";
+
+footerOtrosSitios.append (coronaCo, centroCorona, vajillasCorona, inspirame);
+
+footer.append (footerH4, footerLogoContainer, footerH3, footerOtrosSitios);
 
 
-function slide () {
-   slider.style.transform = "translateX(-75vw)";
-}
-slider.addEventListener("touchmove", slide);
-
-// card1.addEventListener ("click",slide)
-// card2.addEventListener ("click",slide)
-// card3.addEventListener ("click",slide)
-// card4.addEventListener ("click",slide)
-// card5.addEventListener ("click",slide)
