c36c842e0003e800f57e633917289ec2ae2322f5
nico8a54 <nico8a54@hotmail.com>
2021-09-28 11:51:11 -0500

    v54

--- index.js
+++ index.js
@@ -112,11 +112,16 @@ function openModal() {
   function showSlides2(n) {
     var i;
     var slides = document.getElementsByClassName("gallerySlides");
+    var dots = document.getElementsByClassName("demo");
     if (n > slides.length) {slideIndex2 = 1}
     if (n < 1) {slideIndex2 = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active2", " ");
+    }
     slides[slideIndex2-1].style.display = "block";
+    dots[slideIndex2-1].className += " active2";
   }
 
